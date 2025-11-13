2126e8190258f93df444c7fbea4ec17484daa21c
robinaigc <127807057+robinaigc@users.noreply.github.com>
2025-11-14 00:06:31 +0800

    feat:删除多余文件

--- reword-commits.ps1
+++
@@ -1,8 +0,0 @@
-# 修改提交信息的脚本
-param($file)
-
-$content = Get-Content $file -Raw
-$content = $content -replace '^pick 7c663f0', 'reword 7c663f0'
-$content = $content -replace '^pick 274d9e9', 'reword 274d9e9'
-Set-Content $file $content -NoNewline
-
